366c30ad107085d5535edcbf042e373c959db2bb
Kaczmarczyk.Michal <209344@edu.p.lodz.pl>
2019-03-24 23:28:55 +0100

    testy_v2 - modified search method and added testSequenceWithoutElements test

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -3,32 +3,36 @@ import edu.iis.mto.bsearch.SearchResult;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+
 public class BinarySearchTest {
 
+    private final int[] zeroNumberSequence = {};
     private final int[] oneNumberSequence = {6};
     private final int[] threeNumberSequence = {6, 7, 8};
+    private final int KEY_NOT_FOUND_IN_SEQUENCE_INDICATOR = -1;
 
     @Test
     public void testKeyIsFoundInOneNumberSequence() {
         int testKey = 6;
 
-        Assert.assertEquals(1, oneNumberSequence.length);
+        Assert.assertThat(oneNumberSequence.length, is(1));
         SearchResult searchResult = BinarySearch.search(testKey, oneNumberSequence);
 
-        Assert.assertTrue(searchResult.isFound());
-        Assert.assertEquals(testKey, oneNumberSequence[searchResult.getPosition()]);
+        Assert.assertThat(searchResult.isFound(), is(true));
+        Assert.assertThat(oneNumberSequence[searchResult.getPosition()], is(testKey));
     }
 
     @Test
     public void testKeyIsNotFoundInOneNumberSequence() {
         int testKey = 4;
-        int KEY_NOT_FOUND_IN_SEQUENCE_INDICATOR = -1;
 
-        Assert.assertEquals(1, oneNumberSequence.length);
+        Assert.assertThat(oneNumberSequence.length, is(1));
         SearchResult searchResult = BinarySearch.search(testKey, oneNumberSequence);
 
-        Assert.assertFalse(searchResult.isFound());
-        Assert.assertEquals(KEY_NOT_FOUND_IN_SEQUENCE_INDICATOR, searchResult.getPosition());
+        Assert.assertThat(searchResult.isFound(), is(false));
+        Assert.assertThat(searchResult.getPosition(), is(KEY_NOT_FOUND_IN_SEQUENCE_INDICATOR));
     }
 
     @Test
@@ -36,12 +40,12 @@ public class BinarySearchTest {
         int testKey = 6;
         int firstPosition = 0;
 
-        Assert.assertTrue(threeNumberSequence.length > 1);
+        Assert.assertThat(threeNumberSequence.length, greaterThan(1));
         SearchResult searchResult = BinarySearch.search(testKey, threeNumberSequence);
 
-        Assert.assertTrue(searchResult.isFound());
-        Assert.assertEquals(testKey, threeNumberSequence[searchResult.getPosition()]);
-        Assert.assertEquals(firstPosition, searchResult.getPosition());
+        Assert.assertThat(searchResult.isFound(), is(true));
+        Assert.assertThat(threeNumberSequence[searchResult.getPosition()], is(testKey));
+        Assert.assertThat(searchResult.getPosition(), is(firstPosition));
     }
 
     @Test
@@ -49,12 +53,12 @@ public class BinarySearchTest {
         int testKey = 8;
         int lastPosition = threeNumberSequence.length - 1;
 
-        Assert.assertTrue(threeNumberSequence.length > 1);
+        Assert.assertThat(threeNumberSequence.length, greaterThan(1));
         SearchResult searchResult = BinarySearch.search(testKey, threeNumberSequence);
 
-        Assert.assertTrue(searchResult.isFound());
-        Assert.assertEquals(testKey, threeNumberSequence[searchResult.getPosition()]);
-        Assert.assertEquals(lastPosition, searchResult.getPosition());
+        Assert.assertThat(searchResult.isFound(), is(true));
+        Assert.assertThat(threeNumberSequence[searchResult.getPosition()], is(testKey));
+        Assert.assertThat(searchResult.getPosition(), is(lastPosition));
     }
 
     @Test
@@ -62,23 +66,29 @@ public class BinarySearchTest {
         int testKey = 7;
         int middlePosition = (threeNumberSequence.length - 1) / 2;
 
-        Assert.assertTrue(threeNumberSequence.length > 1);
+        Assert.assertThat(threeNumberSequence.length, greaterThan(1));
         SearchResult searchResult = BinarySearch.search(testKey, threeNumberSequence);
 
-        Assert.assertTrue(searchResult.isFound());
-        Assert.assertEquals(testKey, threeNumberSequence[searchResult.getPosition()]);
-        Assert.assertEquals(middlePosition, searchResult.getPosition());
+        Assert.assertThat(searchResult.isFound(), is(true));
+        Assert.assertThat(threeNumberSequence[searchResult.getPosition()], is(testKey));
+        Assert.assertThat(searchResult.getPosition(), is(middlePosition));
     }
 
     @Test
     public void testKeyIsNotFoundInSequenceWithLengthMoreThanOne() {
         int testKey = 4;
-        int KEY_NOT_FOUND_IN_SEQUENCE_INDICATOR = -1;
 
-        Assert.assertTrue(threeNumberSequence.length > 1);
+        Assert.assertThat(threeNumberSequence.length, greaterThan(1));
         SearchResult searchResult = BinarySearch.search(testKey, threeNumberSequence);
 
-        Assert.assertFalse(searchResult.isFound());
-        Assert.assertEquals(KEY_NOT_FOUND_IN_SEQUENCE_INDICATOR, searchResult.getPosition());
+        Assert.assertThat(searchResult.isFound(), is(false));
+        Assert.assertThat(searchResult.getPosition(), is(KEY_NOT_FOUND_IN_SEQUENCE_INDICATOR));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSequenceWithoutElements() {
+        int testKey = 6;
+
+        BinarySearch.search(testKey, zeroNumberSequence);
     }
 }
